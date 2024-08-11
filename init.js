95d125b0c0fed30332840d761219328a030baa03
LeConsulat2 <wooine2@gmail.com>
2024-08-11 16:59:45 +1200

    updating package.json and init.js

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import app from "./app";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config();
 
@@ -8,7 +8,13 @@ mongoose.connect(process.env.DB_URL, {});
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("✅  Connected to DB");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('✅  Connected to DB');
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
 });
